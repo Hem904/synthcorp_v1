87d8f709a0068954f70469880174ead7ab13a884
Hem904 <hemshah9295@gmail.com>
2025-05-11 08:56:04 +0530

    changes

--- Behavioral_Patterns/states/Test.py
+++ Behavioral_Patterns/states/Test.py
@@ -3,14 +3,7 @@ from Machine import Machine
 if __name__ == "__main__":
     machine = Machine()
 
-    # Machine starts in IdleState, then it transitions to ActiveState
-    machine.handle()  # Machine is currently idle. Switching to active state.
-
-    # Machine now in ActiveState, transitions to MaintenanceState
-    machine.handle()  # Machine is active. Switching to maintenance state.
-
-    # Machine now in MaintenanceState, transitions to ErrorState
-    machine.handle()  # Machine is under maintenance. Switching to error state.
-
-    # Machine now in ErrorState, transitions to IdleState
-    machine.handle()  # Machine is in error state. Resetting to idle.
+    machine.handle()  
+    machine.handle()  
+    machine.handle()  
+    machine.handle()  
